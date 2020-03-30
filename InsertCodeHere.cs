d6b71d5b7edecbfbd219ad91e065b5f7656f591a
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:54:58 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WTF
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            Console.WriteLine(MysticNumbers.ThreeNumbers(8,7,9));
+            Console.WriteLine(MysticNumbers.ThreeNumbers(-2, 1, -69));
+            Console.WriteLine(MysticNumbers.TwoNumbers(6,9));
+            Console.WriteLine(MysticNumbers.TwoNumbers(4,4));
+            Console.WriteLine(MysticNumbers.TwoNumbers(7,4));
+            Console.WriteLine(MysticNumbers.FourNumbers(4,2,1,3));
+            Console.WriteLine(MysticNumbers.FourNumbers(4,7,1,3));
+            Console.WriteLine(MysticNumbers.FourNumbers(4,2,8,3));
+            Console.WriteLine(MysticNumbers.FourNumbers(4,2,1,9));
+            
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ MysticNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace WTF
+{
+    public class MysticNumbers
+    {
+        #region Methods
+        public static int ThreeNumbers(int a, int b, int c)
+        {
+            int result;
+
+            if(TwoNumbers(a,b) > c){
+                result = TwoNumbers(a,b);
+            }else{
+                result = c;
+            }
+            return result;
+        }
+        public static int TwoNumbers(int a, int b){
+            int result;
+            if(a > b){
+                result = a;
+            }else{
+                result = b;
+            }
+            return result;
+        }
+        public static int FourNumbers(int a, int b, int c, int d){
+            return TwoNumbers(TwoNumbers(a,b),TwoNumbers(c,d));
+        }
+        #endregion
+    }
+}
